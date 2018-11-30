72e46f232cd156b18b39911c8e126bc0b5ce4934
JOE <joewttx@163.com>
2018-11-30 11:11:41 +0800

    login handler

--- api/handlers.go
+++ api/handlers.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"io"
 	"io/ioutil"
 	"net/http"
 
@@ -36,5 +37,6 @@ func CreateUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 }
 
 func Login(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-
+	uname := p.ByName("user_name")
+	io.WriteString(w, uname)
 }
