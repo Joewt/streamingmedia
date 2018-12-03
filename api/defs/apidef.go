dc43eb079db7d82fce8425cd3bd46c119d333f1b
JOE <joewttx@163.com>
2018-12-03 12:38:23 +0800

    一些数据结构的定义

--- api/defs/apidef.go
+++ api/defs/apidef.go
@@ -1,31 +1,63 @@
 package defs
 
-
 type UserCredential struct {
 	Username string `json:"user_name"`
-	Pwd string `json:"pwd"`
+	Pwd      string `json:"pwd"`
 }
 
 type SignedUp struct {
-	Success bool `json:"success"`
+	Success   bool   `json:"success"`
 	SessionId string `json:"session_id"`
 }
 
+type User struct {
+	Id        int
+	LoginName string
+	pwd       string
+}
+
 type VideoInfo struct {
-	Id string
-	AuthorId int
-	Name string
-	DisplayCtime string
+	Id           string `json:"id"`
+	AuthorId     int    `json:"author_id"`
+	Name         string `json:"name"`
+	DisplayCtime string `json:"display_ctime"`
 }
 
 type Comment struct {
-	Id string
-	VideoId string
-	Author string
-	Content string
+	Id      string `json:"id"`
+	VideoId string `json:"video_id"`
+	Author  string `json:"author"`
+	Content string `json:"content"`
 }
 
 type SimpleSession struct {
 	Username string
-	TTL int64
+	TTL      int64
+}
+
+type UserInfo struct {
+	Id int `json:"id"`
+}
+
+type SignedIn struct {
+	Success   bool   `json:"success"`
+	SessionId string `json:"session_id"`
+}
+
+type VideosInfo struct {
+	Videos []*VideoInfo `json:"videos"`
+}
+
+type NewVideo struct {
+	AuthorId int    `json:"author_id"`
+	Name     string `json:"name"`
+}
+
+type Comments struct {
+	Comments []*Comment `json:"comments"`
+}
+
+type NewComment struct {
+	AuthorId int    `json:"author_id"`
+	Content  string `json:"content"`
 }
--- api/defs/errs.go
+++ api/defs/errs.go
@@ -1,18 +1,18 @@
 package defs
 
 type Err struct {
-	Error string `json:"error"`
+	Error     string `json:"error"`
 	ErrorCode string `json:"error_code"`
 }
 
 type ErrorResponse struct {
 	HttpSC int
-	Error Err
+	Error  Err
 }
 
 var (
-	ErrorRequestBodyParseFailed = ErrorResponse{HttpSC:400,Error:Err{Error:"Request failed",ErrorCode:"0001"}}
-	ErrorNotAuthUser = ErrorResponse{HttpSC:401,Error:Err{Error:"User authentication failed",ErrorCode:"0002"}}
-	ErrorDBError = ErrorResponse{HttpSC:500, Error:Err{Error:"DB ops failed", ErrorCode:"0003"}}
-	ErrorInternalFaults = ErrorResponse{HttpSC:500, Error:Err{Error:"Internal service error", ErrorCode:"0004"}}
+	ErrorRequestBodyParseFailed = ErrorResponse{HttpSC: 400, Error: Err{Error: "Request failed", ErrorCode: "1001"}}
+	ErrorNotAuthUser            = ErrorResponse{HttpSC: 401, Error: Err{Error: "User authentication failed", ErrorCode: "1002"}}
+	ErrorDBError                = ErrorResponse{HttpSC: 500, Error: Err{Error: "DB ops failed", ErrorCode: "1003"}}
+	ErrorInternalFaults         = ErrorResponse{HttpSC: 500, Error: Err{Error: "Internal service error", ErrorCode: "1004"}}
 )
